c33d0f850889a98a29dc0183b2f8e5780af0fb4c
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2022-08-20 00:10:57 +0500

    remove extra code

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -17,39 +17,8 @@ const Header = () => {
             alt=""
           />
         </a>
-
         {/* <!-- Menu / Actions --> */}
         <div className="js-mobile-menu invisible fixed inset-0 z-10 ml-auto items-center bg-white opacity-0 dark:bg-jacarta-800 lg:visible lg:relative lg:inset-auto lg:flex lg:bg-transparent lg:opacity-100 dark:lg:bg-transparent">
-          {/* <!-- Mobile Logo / Menu Close --> */}
-          <div className="t-0 fixed left-0 z-10 flex w-full items-center justify-between bg-white p-6 dark:bg-jacarta-800 lg:hidden">
-            {/* <!-- Mobile Logo --> */}
-            <a href="/" className="shrink-0">
-              <img src="img/logo.png" className="max-h-7 dark:hidden" alt="" />
-              <img
-                src="img/logo_white.png"
-                className="hidden max-h-7 dark:block"
-                alt=""
-              />
-            </a>
-
-            {/* <!-- Mobile Menu Close --> */}
-            <button
-              className="js-mobile-close group ml-2 flex h-10 w-10 items-center justify-center rounded-full border border-jacarta-100 bg-white transition-colors hover:border-transparent hover:bg-accent focus:border-transparent focus:bg-accent dark:border-transparent dark:bg-white/[.15] dark:hover:bg-accent"
-              aria-label="close mobile menu"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-                className="h-4 w-4 fill-jacarta-700 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white"
-              >
-                <path fill="none" d="M0 0h24v24H0z" />
-                <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" />
-              </svg>
-            </button>
-          </div>
-
           {/* <!-- Primary Nav --> */}
           <nav className="navbar w-full">
             <ul className="flex flex-col lg:flex-row">
