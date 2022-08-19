a466d9d3a4f5c1b20c3b3505c95bb836363c6ed1
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2022-08-20 03:39:35 +0500

    run linter command in whole project

--- components/search/RecentSearches.tsx
+++ components/search/RecentSearches.tsx
@@ -1,27 +1,18 @@
-import React from "react";
+import React from 'react';
 
 const RecentSearches = () => {
   return (
     <>
-      <span className="dark:text-jacarta-300">Popular searches: </span>
-      <a
-        href="#"
-        className="text-accent hover:text-jacarta-700 dark:hover:text-white"
-      >
+      <span className='dark:text-jacarta-300'>Popular searches: </span>
+      <a href='#' className='text-accent hover:text-jacarta-700 dark:hover:text-white'>
         cryptopunks
       </a>
       <span>, </span>
-      <a
-        href="#"
-        className="text-accent hover:text-jacarta-700 dark:hover:text-white"
-      >
+      <a href='#' className='text-accent hover:text-jacarta-700 dark:hover:text-white'>
         bored ape yacht club
       </a>
       <span>, </span>
-      <a
-        href="#"
-        className="text-accent hover:text-jacarta-700 dark:hover:text-white"
-      >
+      <a href='#' className='text-accent hover:text-jacarta-700 dark:hover:text-white'>
         moonbirds
       </a>
     </>
--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -1,18 +1,18 @@
-import { ReactNode, useEffect, useState } from "react";
-import Hero from "../components/hero";
-import Header from "../components/header";
+import { ReactNode } from 'react';
+import Hero from '../components/hero';
+import Header from '../components/header';
 
 interface Props {
   pageTitle?: string | null;
   children: ReactNode;
 }
 
-const AppLayout = ({ pageTitle, children }: Props): JSX.Element => {
+const AppLayout = ({ children }: Props): JSX.Element => {
   return (
-    <div id="app">
+    <div id='app'>
       <Header />
       <Hero />
-      {/* <main>{children}</main> */}
+      <main>{children}</main>
     </div>
   );
 };
