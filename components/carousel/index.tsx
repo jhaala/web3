69da77d672400bde6d2aebebc616af7505d5d111
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2022-08-26 21:12:27 +0500

    complete carousel

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow } from 'swiper';
+import { EffectCoverflow, Lazy, Navigation } from 'swiper';
 
 import 'swiper/css';
+import 'swiper/css/lazy';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-coverflow';
 
 const Carousel = () => {
+  const navigationPrevRef = React.useRef(null);
+  const navigationNextRef = React.useRef(null);
+
   return (
     <div className='relative px-6 pb-16 sm:px-0'>
       <div className='swiper coverflow-slider !py-5'>
         <Swiper
           // install Swiper modules
           effect='coverflow'
-          modules={[EffectCoverflow]}
+          modules={[EffectCoverflow, Navigation, Lazy]}
           speed={400}
           loop={true}
           spaceBetween={20}
@@ -38,6 +44,16 @@ const Carousel = () => {
           }}
           preloadImages={true}
           lazy={true}
+          navigation={{
+            prevEl: navigationPrevRef.current,
+            nextEl: navigationNextRef.current,
+          }}
+          onBeforeInit={swiper => {
+            //@ts-ignore
+            swiper.params.navigation.prevEl = navigationPrevRef.current;
+            //@ts-ignore
+            swiper.params.navigation.nextEl = navigationNextRef.current;
+          }}
           className='swiper-wrapper'
         >
           <SwiperSlide className='swiper-slide'>
@@ -68,6 +84,41 @@ const Carousel = () => {
                       </a>
                       <a href='#' className='text-2xs text-accent'>
                         051_Hart
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </SwiperSlide>
+          <SwiperSlide className='swiper-slide'>
+            <article>
+              <div className='block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700'>
+                <figure className='relative'>
+                  <a href='#'>
+                    <img
+                      src='./img/products/item_16_lg.jpg'
+                      alt=''
+                      className='swiper-lazy h-[430px] w-full object-cover'
+                      height='430'
+                      width='379'
+                    />
+                    <div className='swiper-lazy-preloader'></div>
+                  </a>
+                </figure>
+                <div className='p-6'>
+                  <div className='flex'>
+                    <a href='#' className='shrink-0'>
+                      <img src='img/avatars/avatar_22_rounded.jpg' alt='' className='mr-4 h-10 w-10 rounded-full' />
+                    </a>
+                    <div>
+                      <a href='#' className='block'>
+                        <span className='font-display text-lg leading-none text-jacarta-700 hover:text-accent dark:text-white'>
+                          Oceania \\ OVERSEER 017
+                        </span>
+                      </a>
+                      <a href='#' className='text-2xs text-accent'>
+                        MadeByM1KE
                       </a>
                     </div>
                   </div>
@@ -182,6 +233,36 @@ const Carousel = () => {
               </div>
             </article>
           </SwiperSlide>
+          <div
+            ref={navigationPrevRef}
+            className='group absolute top-1/2 left-4 z-10 -mt-6 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-white p-3 text-base shadow-white-volume'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              width='24'
+              height='24'
+              className='fill-jacarta-700 group-hover:fill-accent'
+            >
+              <path fill='none' d='M0 0h24v24H0z' />
+              <path d='M10.828 12l4.95 4.95-1.414 1.414L8 12l6.364-6.364 1.414 1.414z' />
+            </svg>
+          </div>
+          <div
+            ref={navigationNextRef}
+            className='group absolute top-1/2 right-4 z-10 -mt-6 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-white p-3 text-base shadow-white-volume'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              width='24'
+              height='24'
+              className='fill-jacarta-700 group-hover:fill-accent'
+            >
+              <path fill='none' d='M0 0h24v24H0z' />
+              <path d='M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z' />
+            </svg>
+          </div>
         </Swiper>
       </div>
     </div>
