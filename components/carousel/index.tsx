3f7ec2509f7b2e9f97ca8418ca07ef1e0ae288eb
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2022-08-26 21:57:01 +0500

    refactor code and absolute import

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Lazy, Navigation } from 'swiper';
 
+import { SLIDER_CONSTANT } from 'constants/CarouselConstant';
+
 import 'swiper/css';
 import 'swiper/css/lazy';
 import 'swiper/css/navigation';
@@ -55,183 +57,44 @@ const Carousel = () => {
           }}
           className='swiper-wrapper'
         >
-          <SwiperSlide className='swiper-slide'>
-            <article>
-              <div className='block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700'>
-                <figure className='relative'>
-                  <a href='#'>
-                    <img
-                      src='./img/products/item_13_lg.gif'
-                      alt=''
-                      className='swiper-lazy h-[430px] w-full object-cover'
-                      height='430'
-                      width='379'
-                    />
-                    <div className='swiper-lazy-preloader'></div>
-                  </a>
-                </figure>
-                <div className='p-6'>
-                  <div className='flex'>
-                    <a href='#' className='shrink-0'>
-                      <img src='img/avatars/avatar_18_rounded.gif' alt='' className='mr-4 h-10 w-10 rounded-full' />
+          {SLIDER_CONSTANT.map(slider => (
+            <SwiperSlide className='swiper-slide' key={`${slider.nftName}_slider`}>
+              <article>
+                <div className='block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700'>
+                  <figure className='relative'>
+                    <a href='#'>
+                      <img
+                        src={`${slider.nftImage}`}
+                        alt=''
+                        className='swiper-lazy h-[430px] w-full object-cover'
+                        height='430'
+                        width='379'
+                      />
+                      <div className='swiper-lazy-preloader'></div>
                     </a>
-                    <div>
-                      <a href='#' className='block'>
-                        <span className='font-display text-lg leading-none text-jacarta-700 hover:text-accent dark:text-white'>
-                          Etherium NFT Launching Lab
-                        </span>
+                  </figure>
+                  <div className='p-6'>
+                    <div className='flex'>
+                      <a href='#' className='shrink-0'>
+                        <img src={`${slider.userAvatar}`} alt='' className='mr-4 h-10 w-10 rounded-full' />
                       </a>
-                      <a href='#' className='text-2xs text-accent'>
-                        051_Hart
-                      </a>
+                      <div>
+                        <a href='#' className='block'>
+                          <span className='font-display text-lg leading-none text-jacarta-700 hover:text-accent dark:text-white'>
+                            {slider.nftName}
+                          </span>
+                        </a>
+                        <a href='#' className='text-2xs text-accent'>
+                          {slider.creator}
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </article>
-          </SwiperSlide>
-          <SwiperSlide className='swiper-slide'>
-            <article>
-              <div className='block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700'>
-                <figure className='relative'>
-                  <a href='#'>
-                    <img
-                      src='./img/products/item_16_lg.jpg'
-                      alt=''
-                      className='swiper-lazy h-[430px] w-full object-cover'
-                      height='430'
-                      width='379'
-                    />
-                    <div className='swiper-lazy-preloader'></div>
-                  </a>
-                </figure>
-                <div className='p-6'>
-                  <div className='flex'>
-                    <a href='#' className='shrink-0'>
-                      <img src='img/avatars/avatar_22_rounded.jpg' alt='' className='mr-4 h-10 w-10 rounded-full' />
-                    </a>
-                    <div>
-                      <a href='#' className='block'>
-                        <span className='font-display text-lg leading-none text-jacarta-700 hover:text-accent dark:text-white'>
-                          Oceania \\ OVERSEER 017
-                        </span>
-                      </a>
-                      <a href='#' className='text-2xs text-accent'>
-                        MadeByM1KE
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </SwiperSlide>
-          <SwiperSlide className='swiper-slide'>
-            <article>
-              <div className='block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700'>
-                <figure className='relative'>
-                  <a href='#'>
-                    <img
-                      src='./img/products/item_12_lg.jpg'
-                      alt=''
-                      height='430'
-                      width='379'
-                      className='swiper-lazy h-[430px] w-full object-cover'
-                    />
-                    <div className='swiper-lazy-preloader'></div>
-                  </a>
-                </figure>
-                <div className='p-6'>
-                  <div className='flex'>
-                    <a href='#' className='shrink-0'>
-                      <img src='img/avatars/avatar_17_rounded.jpg' alt='' className='mr-4 h-10 w-10 rounded-full' />
-                    </a>
-                    <div>
-                      <a href='#' className='block'>
-                        <span className='font-display text-lg leading-none text-jacarta-700 hover:text-accent dark:text-white'>
-                          Light Bars
-                        </span>
-                      </a>
-                      <a href='#' className='text-2xs text-accent'>
-                        Wow Frens
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </SwiperSlide>
-          <SwiperSlide className='swiper-slide'>
-            <article>
-              <div className='block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700'>
-                <figure className='relative'>
-                  <a href='#'>
-                    <img
-                      src='./img/pixel.jpg'
-                      data-src='./img/products/item_14_lg.jpg'
-                      alt=''
-                      className='swiper-lazy h-[430px] w-full object-cover'
-                      height='430'
-                      width='379'
-                    />
-                    <div className='swiper-lazy-preloader'></div>
-                  </a>
-                </figure>
-                <div className='p-6'>
-                  <div className='flex'>
-                    <a href='#' className='shrink-0'>
-                      <img src='img/avatars/avatar_19_rounded.jpg' alt='' className='mr-4 h-10 w-10 rounded-full' />
-                    </a>
-                    <div>
-                      <a href='#' className='block'>
-                        <span className='font-display text-lg leading-none text-jacarta-700 hover:text-accent dark:text-white'>
-                          Amazing NFT art
-                        </span>
-                      </a>
-                      <a href='#' className='text-2xs text-accent'>
-                        Lila Spacex
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </SwiperSlide>
-          <SwiperSlide className='swiper-slide'>
-            <article>
-              <div className='block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700'>
-                <figure className='relative'>
-                  <a href='#'>
-                    <img
-                      src='./img/pixel.jpg'
-                      data-src='./img/products/item_15_lg.jpg'
-                      alt=''
-                      className='swiper-lazy h-[430px] w-full object-cover'
-                      height='430'
-                      width='379'
-                    />
-                    <div className='swiper-lazy-preloader'></div>
-                  </a>
-                </figure>
-                <div className='p-6'>
-                  <div className='flex'>
-                    <a href='#' className='shrink-0'>
-                      <img src='img/avatars/avatar_20_rounded.jpg' alt='' className='mr-4 h-10 w-10 rounded-full' />
-                    </a>
-                    <div>
-                      <a href='#' className='block'>
-                        <span className='font-display text-lg leading-none text-jacarta-700 hover:text-accent dark:text-white'>
-                          SwagFox#133
-                        </span>
-                      </a>
-                      <a href='#' className='text-2xs text-accent'>
-                        Bored Bunny
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </SwiperSlide>
+              </article>
+            </SwiperSlide>
+          ))}
+
           <div
             ref={navigationPrevRef}
             className='group absolute top-1/2 left-4 z-10 -mt-6 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-white p-3 text-base shadow-white-volume'
