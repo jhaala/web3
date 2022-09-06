d095ba09abd9c4c65338c45b1c9fda51dfc54e88
Jason Haala <jason@haalabrands.com>
2022-09-05 21:11:21 -0400

    Fixes formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import '../styles/app.css';
-import type { AppProps } from 'next/app';
+import type {AppProps} from 'next/app';
 import AppLayout from '../layouts/AppLayout';
 
-function App({ Component, pageProps }: AppProps) {
-  return (
-    <AppLayout pageTitle={`Web3 App`}>
-      <Component {...pageProps} />
-    </AppLayout>
-  )
+function App({Component, pageProps}: AppProps) {
+	return (
+		<AppLayout pageTitle={`Web3 App`}>
+			<Component {...pageProps} />
+		</AppLayout>
+	);
 }
 
-export default App
+export default App;
