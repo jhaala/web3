743f4dc27b1250ad3f30bd8b5bdb701dbd84103a
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2022-09-14 18:06:34 +0500

    update heading component

--- components/heading/index.tsx
+++ components/heading/index.tsx
@@ -8,17 +8,16 @@ interface HeadingType {
 
 const Heading: React.FC<HeadingType> = ({ text, type = 'title', className = '' }) => {
   return (
-    <div
+    <span
       className={classnames(`font-semibold ${className}`, {
         'text-4xl': type === 'title',
         'text-2xl': type === 'heading',
         'text-lg': type === 'subheading',
         'text-base': type === 'base',
-        'mb-3': !className.includes('mb-'), // custom margin bottom doesn't always overrides
       })}
     >
       {text}
-    </div>
+    </span>
   );
 };
 
