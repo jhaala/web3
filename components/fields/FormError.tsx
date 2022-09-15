152351b56d94f0fc30d219618be2e9e53e01b5d2
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2022-09-15 05:38:27 +0500

    change formError icons to cross icons

--- components/fields/FormError.tsx
+++ components/fields/FormError.tsx
@@ -1,12 +1,12 @@
-import { AiOutlineWarning } from 'react-icons/ai';
+import { ImCross } from 'react-icons/im';
 
 const FormError: React.FC<{ error?: React.ReactNode }> = ({ error }) => {
   if (!error) return null;
 
   return (
-    <div className='flex items-center gap-1 mt-1 ml-2 text-xs text-red-500'>
+    <div className='flex items-center gap-1 mt-1 ml-2 text-xs text-red'>
       <div>
-        <AiOutlineWarning />
+        <ImCross />
       </div>
       <p>{error}</p>
     </div>
